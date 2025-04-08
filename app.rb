ba5f12dc8630e544dfb5333521ca815fad5037a5
georgie-porgie-pudding-pie <gkobalya@chicagobooth.edu>
2025-04-08 00:13:57 +0000

    FIRST

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/process_roll") do
+  @number_of_dice = params.fetch("number_dice").to_i
+  @number_of_sides = params.fetch("number_sides").to_i
+
+  @rolls = []
+
+  @number_of_dice.times do
+    die = rand(1..@number_of_sides)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
 end
